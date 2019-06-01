7ed37caf615322be843a7b411ac069d2a4d01dd0
MARCELO FAVIERO GONZALEZ <ALU201830833@uniritter.local>
2019-05-31 21:15:06 -0300

    Initial commit

---
+++ src/main/java/edu/unirriter/pdametrics/PdaMetricsApplication.java
@@ -0,0 +1,13 @@
+package edu.unirriter.pdametrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdaMetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdaMetricsApplication.class, args);
+	}
+
+}
